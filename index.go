a07ecf45ffb8e91c38c999673f3a0a3a88434d08
Caleb Williams <calebjwilliams7@gmail.com>
2019-03-27 00:37:32 -0500

    Make sure it can be deployed to Now, better dimension and color handling

--- index.go
+++ index.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"image"
@@ -12,44 +13,109 @@ import (
 	"math/rand"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 var errInvalidFormat = errors.New("invalid format")
 
-// Stolen from https://stackoverflow.com/a/54200713
-func ParseHexColor(s string) (c color.RGBA, err error) {
-	c.A = 0xff
+func Handler(w http.ResponseWriter, r *http.Request) {
+	config := ImageConfig{}
 
-	if s[0] != '#' {
-		return c, errInvalidFormat
+	query_params := r.URL.Query()
+
+	fmt.Println("*** NEW REQUEST ***")
+	fmt.Println("query params:")
+	for k, v := range query_params {
+		fmt.Printf("key[%s] value[%s]\n", k, v)
 	}
 
-	hexToByte := func(b byte) byte {
-		switch {
-		case b >= '0' && b <= '9':
-			return b - '0'
-		case b >= 'a' && b <= 'f':
-			return b - 'a'
-		case b >= 'A' && b <= 'F':
-			return b - 'A'
+	if width, err := strconv.Atoi(query_params.Get("width")); err == nil {
+		config.Width = width
+	}
+
+	if height, err := strconv.Atoi(query_params.Get("height")); err == nil {
+		if height == 0 {
+			config.Height = config.Width
+		} else {
+			config.Height = height
+
+			if config.Width == 0 {
+				config.Width = config.Height
+			}
 		}
-		err = errInvalidFormat
-		return 0
+	} else {
+		config.Height = config.Width
 	}
 
-	switch len(s) {
-	case 7:
-		c.R = hexToByte(s[1])<<4 + hexToByte(s[2])
-		c.G = hexToByte(s[3])<<4 + hexToByte(s[4])
-		c.B = hexToByte(s[5])<<4 + hexToByte(s[6])
-	case 4:
-		c.R = hexToByte(s[1]) * 17
-		c.G = hexToByte(s[2]) * 17
-		c.B = hexToByte(s[3]) * 17
-	default:
-		err = errInvalidFormat
+	/* Fail on 0x0 dimensions */
+	if config.Height == 0 && config.Width == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+	} else {
+		c := query_params.Get("color")
+		fmt.Printf("%T, %v\n", c, c)
+
+		if len(c) == 0 {
+			fmt.Println("color query param not found")
+		} else {
+			config.Color = c
+		}
+
+		fmt.Printf("ImageConfig: %+v\n", config)
+
+		img := NewImage(config)
+
+		img_buffer := EncodeImageToBuffer(img, "jpeg")
+
+		// Set headers
+		w.Header().Set("Content-Type", "image/jpeg")
+		w.Header().Set("Content-Length", strconv.Itoa(len(img_buffer.Bytes())))
+		w.WriteHeader(http.StatusOK)
+
+		if _, err := w.Write(img_buffer.Bytes()); err != nil {
+			fmt.Println("unable to write image...")
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 	}
-	return
+}
+
+func ParseHexColor(s string) color.RGBA {
+	normalize := func(s string) (string, error) {
+		var b strings.Builder
+
+		switch len(s) {
+		case 8:
+			b.WriteString(s)
+		case 6:
+			b.WriteString(s)
+			b.WriteString("FF")
+		case 3:
+			for _, char := range s {
+				b.WriteRune(char)
+				b.WriteRune(char)
+			}
+			b.WriteString("FF")
+		default:
+			return s, errors.New("couldn't normalize the color string")
+		}
+
+		s = b.String()
+
+		fmt.Println("normalized color:", s)
+
+		return s, nil
+	}
+
+	colorStr, err := normalize(s)
+	if err != nil {
+		return RandomColor()
+	}
+
+	b, err := hex.DecodeString(colorStr)
+	if err != nil {
+		return RandomColor()
+	}
+
+	return color.RGBA{b[0], b[1], b[2], b[3]}
 }
 
 func RandomChannelValue() uint8 {
@@ -67,26 +133,16 @@ func RandomColor() color.RGBA {
 type ImageConfig struct {
 	Width  int
 	Height int
-	Color  string "black"
-}
-
-func NewImageConfig() ImageConfig {
-	image_config := ImageConfig{}
-	image_config.Color = "black"
-	return image_config
+	Color  string
 }
 
 func NewImage(image_config ImageConfig) *image.RGBA {
 	upLeft := image.Point{0, 0}
 	downRight := image.Point{image_config.Width, image_config.Height}
 
-	img_color, err := ParseHexColor(image_config.Color)
+	img_color := ParseHexColor(image_config.Color)
 
-	if err != nil {
-		fmt.Printf("unable to parse color %s\n", image_config.Color)
-		// img_color := color.RGBA{10, 10, 10, 255}
-		img_color = RandomColor()
-	}
+	fmt.Printf("Image color: %+v\n", img_color)
 
 	img := image.NewRGBA(image.Rectangle{upLeft, downRight})
 	draw.Draw(img, img.Bounds(), &image.Uniform{img_color}, image.ZP, draw.Src)
@@ -106,66 +162,17 @@ func EncodeImageToBuffer(img *image.RGBA, format string) *bytes.Buffer {
 	return buffer
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	config := NewImageConfig()
+/* Uncomment to develop locally */
 
-	query_params := r.URL.Query()
+// func main() {
+// 	http.HandleFunc("/", Handler)
 
-	fmt.Println("query params:")
-	for k, v := range query_params {
-		fmt.Printf("key[%s] value[%s]\n", k, v)
-	}
+// 	port := 8080
 
-	if width, err := strconv.Atoi(query_params.Get("width")); err == nil {
-		// fmt.Printf("%T, %v\n", width, width)
-		config.Width = width
-	}
+// 	// Handle static files
+// 	fs := http.FileServer(http.Dir("static/"))
+// 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	if height, err := strconv.Atoi(query_params.Get("height")); err == nil {
-		// fmt.Printf("%T, %v\n", height, height)
-		config.Height = height
-	}
-
-	c := query_params.Get("color")
-	// fmt.Printf("%T, %v\n", c, c)
-
-	if c == "" {
-		fmt.Println("color query param not found")
-		// config.Color = "#123321"
-		// config.Color = strconv.Parse
-	} else {
-		if c[0] != '#' {
-			c = "#" + c
-		}
-		config.Color = c
-	}
-
-	fmt.Printf("ImageConfig: %+v\n", config)
-
-	// fmt.Fprintf(w, "<h1>Hello from Go!")
-
-	img := NewImage(config)
-
-	img_buffer := EncodeImageToBuffer(img, "jpeg")
-
-	// Set headers
-	w.Header().Set("Content-Type", "image/jpeg")
-	w.Header().Set("Content-Length", strconv.Itoa(len(img_buffer.Bytes())))
-
-	if _, err := w.Write(img_buffer.Bytes()); err != nil {
-		fmt.Println("unable to write image...")
-	}
-}
-
-func main() {
-	http.HandleFunc("/", Handler)
-
-	port := 8080
-
-	// Handle static files
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	fmt.Println(fmt.Sprintf("Server listening at http://localhost:%d", port))
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
-}
+// 	fmt.Println(fmt.Sprintf("Server listening at http://localhost:%d", port))
+// 	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+// }
